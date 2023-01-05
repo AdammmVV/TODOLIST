ef0f72653d13513fde61480d4aa22432b9788a59
Adammm <adammm.web@gmail.com>
2023-01-05 22:24:57 +0300

    create component Counter and useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
 
+export const Counter = () => {
+
+    let [data, setData] = useState(5)
+
+    // let arr = useState(5);
+    // let data = arr[0];
+    // let setData = arr[1]
+
+    return <div onClick={() => {
+        setData(data + 1)
+    }}>
+        {data}
+        <button>+</button>
+    </div>
+}
+
 function App() {
-    const shapka = 'What to learn';
-    const shapka1 = 'What to learn111';
-    const tasks1 = [
-        { id: 1, title: "HTML&CSS", isDone: true },
-        { id: 2, title: "JS", isDone: true },
-        { id: 3, title: "ReactJS", isDone: false }
-    ]
-    const tasks2 = [
-        { id: 1, title: "Hello world", isDone: true },
-        { id: 2, title: "I am Happy", isDone: false },
-        { id: 3, title: "Yo", isDone: false },
-        { id: 4, title: "Yo", isDone: true }
-    ]
+    const header = 'What to learn';
+
+    let [tasks, setTasks] = useState([
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false},
+        {id: 4, title: "Redux", isDone: false}
+    ]);
+    // const tasks2 = [
+    //     { id: 1, title: "Hello world", isDone: true },
+    //     { id: 2, title: "I am Happy", isDone: false },
+    //     { id: 3, title: "Yo", isDone: false },
+    //     { id: 4, title: "Yo", isDone: true }
+    // ]
+
 
     return (
         <div className="App">
-            <Todolist shapka={shapka} tasks={tasks1}/>
-            <Todolist shapka={shapka1} tasks={tasks2}/>
+            <Todolist header={header} tasks={tasks}/>
         </div>
     );
 }
