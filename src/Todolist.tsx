edcec50a0e7447c3aef4c9f5174b3f56cee792ac
Adammm <adammm.web@gmail.com>
2023-02-07 22:48:44 +0300

    change logic for EditableSpan.tsx in Todolist.tsx

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,6 +1,7 @@
 import React, {ChangeEvent} from 'react';
 import {FilterValuesType} from './App';
 import {AddItemForm} from "./AddItemForm";
+import {EditableSpan} from "./EditableSpan";
 
 export type TaskType = {
     id: string
@@ -18,6 +19,7 @@ type PropsType = {
     changeTaskStatus: (id: string, isDone: boolean, todolistId: string) => void
     removeTodolist: (id: string) => void
     filter: FilterValuesType
+    changeTAskTitle: (id: string, title: string, todolistId: string) => void
 }
 
 export function Todolist(props: PropsType) {
@@ -30,6 +32,10 @@ export function Todolist(props: PropsType) {
     const onAllClickHandler = () => props.changeFilter("all", props.id);
     const onActiveClickHandler = () => props.changeFilter("active", props.id);
     const onCompletedClickHandler = () => props.changeFilter("completed", props.id);
+
+    const changeTitle = (id: string, title: string) => {
+        props.changeTAskTitle(id, title, props.id)
+    }
 
     return <div>
         <h3> {props.title}
@@ -45,9 +51,14 @@ export function Todolist(props: PropsType) {
                         props.changeTaskStatus(t.id, newIsDoneValue, props.id);
                     }
 
+                    const changeTitleHAndler = (title: string) => {
+                        changeTitle(t.id, title)
+                    }
+
                     return <li key={t.id} className={t.isDone ? "is-done" : ""}>
                         <input type="checkbox" onChange={onChangeHandler} checked={t.isDone}/>
-                        <span>{t.title}</span>
+                        {/*<span>{t.title}</span>*/}
+                        <EditableSpan title={t.title} changeTitle={changeTitleHAndler} />
                         <button onClick={onClickHandler}>x</button>
                     </li>
                 })
