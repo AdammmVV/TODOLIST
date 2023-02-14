1ee7f39641d0fa70f3001353c0d758fecd1760e2
Adammm <adammm.web@gmail.com>
2023-02-14 21:55:13 +0300

    add material-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createRoot } from 'react-dom/client';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {amber, purple} from "@mui/material/colors";
+import {CssBaseline} from "@mui/material";
 
-ReactDOM.render(<App />,  document.getElementById('root'));
+
+const  them = createTheme({
+    palette: {
+        primary: amber,
+        secondary: purple,
+        mode: 'dark'
+    }
+})
+
+const container  = document.getElementById('root') as HTMLElement
+const root = createRoot(container);
+root.render(
+    <ThemeProvider theme={them}>
+        <CssBaseline/>
+        <App />
+    </ThemeProvider>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
